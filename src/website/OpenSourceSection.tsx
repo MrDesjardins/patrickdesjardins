45597c48e5eef1a9dce30a7e3b5049cc9563cd90
Patrick Desjardins <pdesjardins@netflix.com>
2021-12-13 13:09:38 -0800

    - Add the open source project on the Hilbert Curve - Add more padding for the paragraph. When full screen on the MacOS (retina) the paragraphs were touching.

--- src/website/OpenSourceSection.tsx
+++ src/website/OpenSourceSection.tsx
@@ -2,15 +2,32 @@ import clsx from "clsx";
 import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import {
-  sectionMainpage, sectionVisualOne, sectionFixedWitdh,
-  cardFixedWidthImageImg
+  sectionMainpage,
+  sectionVisualOne,
+  sectionFixedWitdh,
+  cardFixedWidthImageImg,
 } from "../website/index.module.css";
 import { CardFixedWidth } from "./CardFixedWidth";
 export const OpenSourceSection = (): JSX.Element => {
   return (
-    <section id="open-source" className={clsx(sectionMainpage, sectionVisualOne)}>
+    <section
+      id="open-source"
+      className={clsx(sectionMainpage, sectionVisualOne)}
+    >
       <header>Open Source</header>
       <div className={sectionFixedWitdh}>
+        <CardFixedWidth
+          title="TypeScript Hilbert Curve"
+          description="Main maintainer. TypeScript implementation of the Hilbert Curve algorithm. The project contains Github workflow that build the project for CommonJS and EcmaScript, has a matrix of NodeJS/OS version, unit tests and performance tests."
+          link="https://github.com/MrDesjardins/hilbert-curve-ts/"
+          image={
+            <StaticImage
+              className={cardFixedWidthImageImg}
+              alt="Screenshot of the a 2d representation of a third order Hilbert Curve"
+              src="../images/portfolio/hilbertcurve.png"
+            />
+          }
+        />
         <CardFixedWidth
           title="Data Access Gateway Chrome Extension"
           description="Main maintainer. The goal of this Chrome Extension is to receive statistics about how the data is fetched and saved by the library. Build with TypeScript."
