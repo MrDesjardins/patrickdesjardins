9810a2c1e3bc3339a3885ae915221d7fb85417a8
Patrick <mrdesjardins@gmail.com>
2022-07-16 20:12:06 -0700

    Add the new article in the TocOfAzure

--- src/blogcomponents/TocAzureContainerSeries.tsx
+++ src/blogcomponents/TocAzureContainerSeries.tsx
@@ -4,8 +4,21 @@ export const TocAzureContainerSeries = () => {
     <div>
       <h2>Azure Blog Posts: Docker Images & Kubernetes</h2>
       <ol>
-        <li><a href="azure-docker-container-repository">Azure Docker Container Locally</a></li>
-        <li><a href="azure-docker-container-repository-github">Azure Docker Container with Github</a></li>
+        <li>
+          <a href="azure-docker-container-repository">
+            Azure Docker Container Locally
+          </a>
+        </li>
+        <li>
+          <a href="azure-docker-container-repository-github">
+            Azure Docker Container with Github Workflow (actions)
+          </a>
+        </li>
+        <li>
+          <a href="azure-intro-kubernetes">
+            Azure Kubernetes with Github Workflow (actions)
+          </a>
+        </li>
       </ol>
     </div>
   );
