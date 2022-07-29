7ea2af915fe03b92151bab395c6b8e13087c1cfd
Patrick <mrdesjardins@gmail.com>
2022-07-28 17:09:42 -0700

    - Reschedule articles - ToC with some JavaScript to not display future articles

--- src/blogcomponents/TocAzureContainerSeries.tsx
+++ src/blogcomponents/TocAzureContainerSeries.tsx
@@ -1,29 +1,40 @@
 import * as React from "react";
 export const TocAzureContainerSeries = () => {
+  const currentDate = new Date();
   return (
     <div>
       <h2>Azure Blog Posts: Docker Images & Kubernetes</h2>
       <ol>
-        <li>
-          <a href="azure-docker-container-repository">
-            Azure Docker Container Locally
-          </a>
-        </li>
-        <li>
-          <a href="azure-docker-container-repository-github">
-            Azure Docker Container with Github Workflow (actions)
-          </a>
-        </li>
-        <li>
-          <a href="azure-intro-kubernetes">
-            Azure Kubernetes with Github Workflow (actions)
-          </a>
-        </li>
-        <li>
-          <a href="azure-intro-kubernetes">
-            How to Access your Web Application on Kubernetes Azure
-          </a>
-        </li>
+        {currentDate >= new Date(2022, 7 - 1, 14) ? (
+          <li>
+            <a href="azure-docker-container-repository">
+              How to host Docker images on Microsoft Azure
+            </a>
+          </li>
+        ) : null}
+        {currentDate >= new Date(2022, 7 - 1, 28) ? (
+          <li>
+            <a href="azure-docker-container-repository-github">
+              How to use Kubernetes with Microsoft Azure and GitHub and how to
+              debug if it does not workout
+            </a>
+          </li>
+        ) : null}
+        {currentDate >= new Date(2022, 8 - 1, 5) ? (
+          <li>
+            <a href="azure-intro-kubernetes">
+              An Introduction to Microsoft Azure and Kubernetes using Helm and
+              Docker Images
+            </a>
+          </li>
+        ) : null}
+        {currentDate >= new Date(2022, 8 - 1, 23) ? (
+          <li>
+            <a href="azure-kubernetes-public-access">
+              How to Access your Web Application on Kubernetes Azure
+            </a>
+          </li>
+        ) : null}
       </ol>
     </div>
   );
