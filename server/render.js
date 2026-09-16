1f8d10305b6b1378ad1636c6080a0ec014038396
MrDesjardins <MrDesjardins@users.noreply.github.com>
2026-09-16 02:18:52 +0000

    deploy: 060a7a6bdecbe97f0b52f783f622409fcb1e789a

--- server/render.js
+++ server/render.js
@@ -43,8 +43,8 @@ const styles$c = {
   imageWrapper
 };
 const gaMeasurementId = "G-6EH8HNYV13";
-const buildCommit = "db92e71606214ecc982d28b419fdf03a098654b2";
-const buildTime = "35016427246";
+const buildCommit = "060a7a6bdecbe97f0b52f783f622409fcb1e789a";
+const buildTime = "35047338638";
 function isTelemetryEnabled() {
   return typeof window !== "undefined" && typeof window.gtag === "function" && gaMeasurementId !== void 0 && gaMeasurementId.length > 0;
 }
